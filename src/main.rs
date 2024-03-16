3021457f8a66630c9923ae841895a648c0d01cf5
thesayfulla <mirxoliqov.sayfulla@gmail.com>
2024-03-16 12:44:50 +0500

    added more cats

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ struct Options {
     #[structopt(default_value="Meow!")]
     message: String,
 
-    #[structopt(short = "d", long = "dead")]
-    dead: bool,
-
     #[structopt(short = "f", long = "file", parse(from_os_str))]
     catfile: Option<std::path::PathBuf>,
 }
@@ -16,26 +13,24 @@ struct Options {
 fn main() {
     let options = Options::from_args();
     let message = options.message;
-    let eye = if options.dead { "x" } else { "o" };
-
+    
     if message.to_lowercase() == "woof" {
         eprintln!("A cat shouldn't bark like a dog.")
     }
-
-    println!("{}", message.bright_yellow().underline().on_purple());
 
     match &options.catfile {
         Some(path) => {
             let cat_template = std::fs::read_to_string(path)
                 .expect(&format!("could not read file {:?}", path));
-            let cat_picture = cat_template.replace("{eye}", eye);
+            let cat_picture = cat_template.replace("{message}", &message.bright_red().underline());
             println!("{}", &cat_picture);
         }
         None => {
+            println!("{}", message);
             println!(" \\");
             println!("  \\");
             println!("     /\\_/\\");
-            println!("    ( {eye} {eye} )", eye=eye.red().bold());
+            println!("    ( O O )");
             println!("    =( I )=");
         }
     }
